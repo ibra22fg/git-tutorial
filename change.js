a5470b3c24cdca81c62c5dc50b185dfe816ff627
Ibrahim <ibrahim@example.com>
2025-12-08 19:11:41 +0200

    Version 2 with additional line

--- change.js
+++ change.js
@@ -1,3 +1,4 @@
 console.log("change.js");
 console.log("Version 1");
 console.log("Added in amend");
+console.log("Added in amend");
--- config.js
+++ config.js
@@ -1,3 +1,2 @@
 console.log("config.js");
 console.log("Version 1");
-console.log("Version 2");
